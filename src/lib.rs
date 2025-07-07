0eaa423d83c9891ce6b7d85d828529ed24e4639d
Ole Kristian Sandum <oksandum@gmail.com>
2025-07-07 13:17:12 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -10,10 +10,6 @@ pub fn mul(left: u64, right: u64) -> u64 {
     left * right
 }
 
-pub fn div(left: u64, right: u64) -> u64 {
-    left / right
-}
-
 pub fn r#mod(left: u64, right: u64) -> u64 {
     left % right
 }
